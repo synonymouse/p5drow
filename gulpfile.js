9af4a1268b7efe4ec0a3c37f7be29956c9c27929
[synonymouse] <[soulfix.lazy@gmail.com]>
2016-02-21 22:58:06 +0300

    adding jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,67 @@
+'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
-var bourbon = require("node-bourbon").includePaths;
-var neat = require("node-neat").includePaths;
+var injector = require('bs-html-injector');
+var jade = require('gulp-jade');
+var bourbon = require('node-bourbon').includePaths;
+var neat = require('node-neat').includePaths;
 var reload = browserSync.reload;
 
 var src = {
-   scss: 'src/scss/*.scss',
-   css:  'src/css',
-   html: 'src/*.html'
+  src: 'src',
+  css: 'src/css',
+  scss: 'src/scss/*.scss',
+  html: 'src/*.html',
+  jade: 'src/*.jade'
 };
+
+// Jade convert
+
+gulp.task('jade', function () {
+  gulp.src(src.jade)
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest(src.src))
+});
 
 // Sass convert
 
-gulp.task("sass", function() {
-	gulp.src(src.scss)
-			.pipe(sass({
-				includePaths: bourbon,
-				includePaths: neat
-			}))
-			.pipe(gulp.dest(src.css))
-			.pipe(browserSync.reload({
-				stream: true
-			}))
+gulp.task('sass', function () {
+  gulp.src(src.scss)
+    .pipe(sass({
+      includePaths: bourbon,
+      includePaths: neat
+    }))
+    .pipe(gulp.dest(src.css))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 // Browsersync server
 
-gulp.task('serve', ['sass'], function() {
- browserSync({
-   server: {
-     baseDir: 'src'
-   }
- });
+gulp.task('server', ['sass'], function () {
+  browserSync.use(injector, {
+        files: 'src/*.html'
+    });
+  browserSync({
+    server: {
+      baseDir: 'src'
+    },
+    notify: false
+  });
+});
 
 //Watch
 
- gulp.watch(src.scss, ['sass']);
-   gulp.watch(src.html).on('change', browserSync.reload);
+gulp.task('watch', function () {
+  gulp.watch(src.scss, ['sass']);
+  gulp.watch(src.jade, ['jade']);
 });
 
+// Default Task
+
+gulp.task('default', ['server', 'watch']);
